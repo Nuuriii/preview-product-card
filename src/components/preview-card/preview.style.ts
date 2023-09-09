f9fadb6da5d5f642b58420ac8147818a06fbaca3
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-09 07:37:10 +0700

    feat: add husky and set up pre-commit

--- src/components/preview-card/preview.style.ts
+++ src/components/preview-card/preview.style.ts
@@ -1,75 +1,75 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-   display: flex;
-   height: 100vh;
-   justify-content: center;
-   align-items: center;
-   background-color: hsl(30, 38%, 92%);
-   padding: 0 1rem;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: hsl(30, 38%, 92%);
+  padding: 0 1rem;
 `;
 
 export const Wrapper = styled.div`
-   background-color: #fff;
-   border-radius: 0.7rem;
-   overflow: hidden;
+  background-color: #fff;
+  border-radius: 0.7rem;
+  overflow: hidden;
 `;
 
 export const ImageWrapper = styled.div``;
 
 export const Image = styled.img`
-   height: 100%;
-   width: 100%;
+  height: 100%;
+  width: 100%;
 `;
 
 export const Description = styled.div`
-   padding: 1.5rem 1rem;
-   display: flex;
-   flex-direction: column;
+  padding: 1.5rem 1rem;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Type = styled.h4`
-   color: hsl(228, 12%, 48%);
+  color: hsl(228, 12%, 48%);
 `;
 
 export const Title = styled.h1`
-   color: hsl(212, 21%, 14%);
-   margin: 1rem 0;
+  color: hsl(212, 21%, 14%);
+  margin: 1rem 0;
 `;
 
 export const Info = styled.p`
-   color: hsl(228, 12%, 48%);
+  color: hsl(228, 12%, 48%);
 `;
 
 export const PriceWrapper = styled.div`
-   display: flex;
-   margin: 1rem 0;
-   align-items: center;
+  display: flex;
+  margin: 1rem 0;
+  align-items: center;
 `;
 
 export const Price = styled.p`
-   color: hsl(158, 36%, 37%);
-   font-size: 1.7rem;
-   font-weight: 900;
-   margin: 0 0.9rem 0 0;
+  color: hsl(158, 36%, 37%);
+  font-size: 1.7rem;
+  font-weight: 900;
+  margin: 0 0.9rem 0 0;
 `;
 
 export const DiscPrice = styled.small`
-   color: hsl(228, 12%, 48%);
-   font-size: 0.9rem;
-   text-decoration: line-through;
+  color: hsl(228, 12%, 48%);
+  font-size: 0.9rem;
+  text-decoration: line-through;
 `;
 
 export const Button = styled.button`
-   background-color: hsl(158, 36%, 37%);
-   color: white;
-   padding: 1rem 0;
-   display: flex;
-   justify-content: center;
-   border: none;
-   font-weight: 900;
-   border-radius: 0.5rem;
-   img {
-      margin: 0 0.6rem 0 0;
-   }
+  background-color: hsl(158, 36%, 37%);
+  color: white;
+  padding: 1rem 0;
+  display: flex;
+  justify-content: center;
+  border: none;
+  font-weight: 900;
+  border-radius: 0.5rem;
+  img {
+    margin: 0 0.6rem 0 0;
+  }
 `;
--- src/components/preview-card/preview.tsx
+++ src/components/preview-card/preview.tsx
@@ -1,44 +1,44 @@
-import MobilePreview from "../../assets/image-product-mobile.jpg";
-import Cart from "../../assets/icon-cart.svg";
+import MobilePreview from '../../assets/image-product-mobile.jpg';
+import Cart from '../../assets/icon-cart.svg';
 import {
-   Container,
-   Wrapper,
-   ImageWrapper,
-   Image,
-   Description,
-   Type,
-   Title,
-   Info,
-   PriceWrapper,
-   Price,
-   DiscPrice,
-   Button,
-} from "./preview.style";
+  Container,
+  Wrapper,
+  ImageWrapper,
+  Image,
+  Description,
+  Type,
+  Title,
+  Info,
+  PriceWrapper,
+  Price,
+  DiscPrice,
+  Button,
+} from './preview.style';
 
 export const Preview = () => {
-   return (
-      <Container>
-         <Wrapper>
-            <ImageWrapper>
-               <Image src={MobilePreview} />
-            </ImageWrapper>
-            <Description>
-               <Type>Perfume</Type>
-               <Title>Gabrielle Essence Eau De Parfum</Title>
-               <Info>
-                  A floral, solar and voluptuous interpretation composed by
-                  Olivier Polge, Perfumer-Creator for the House of CHANEL.
-               </Info>
-               <PriceWrapper>
-                  <Price>$149.99</Price>
-                  <DiscPrice>$169.99</DiscPrice>
-               </PriceWrapper>
-               <Button>
-                  <img src={Cart} alt='' />
-                  <span> Add to Cart</span>
-               </Button>
-            </Description>
-         </Wrapper>
-      </Container>
-   );
+  return (
+    <Container>
+      <Wrapper>
+        <ImageWrapper>
+          <Image src={MobilePreview} />
+        </ImageWrapper>
+        <Description>
+          <Type>Perfume</Type>
+          <Title>Gabrielle Essence Eau De Parfum</Title>
+          <Info>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </Info>
+          <PriceWrapper>
+            <Price>$149.99</Price>
+            <DiscPrice>$169.99</DiscPrice>
+          </PriceWrapper>
+          <Button>
+            <img src={Cart} alt='' />
+            <span> Add to Cart</span>
+          </Button>
+        </Description>
+      </Wrapper>
+    </Container>
+  );
 };
