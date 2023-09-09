3e53cfbe9d05a120697ebd3d12d082373cf6b3f9
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-09 10:33:56 +0700

    fix: fix responsive in mobile and desktop

--- src/components/preview-card/preview.style.ts
+++ src/components/preview-card/preview.style.ts
@@ -25,19 +25,55 @@ export const Wrapper = styled.div`
   background-color: #fff;
   border-radius: 0.7rem;
   overflow: hidden;
+  width: 20rem;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    width: 40rem;
+  }
 `;
 
-export const ImageWrapper = styled.div``;
+export const ImageWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+`;
 
-export const Image = styled.img`
+export const MobileImage = styled.img`
+  display: block;
   height: 100%;
   width: 100%;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const DekstopImage = styled.img`
+  height: 100%;
+  display: none;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
 `;
 
 export const Description = styled.div`
   padding: 1.5rem 1rem;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+`;
+
+export const DescWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   flex-direction: column;
+  @media screen and (min-width: 768px) {
+    //background-color: red;
+    height: 25rem;
+  }
 `;
 
 export const Type = styled.h5`
@@ -90,5 +126,11 @@ export const Button = styled.button`
   border-radius: 0.5rem;
   img {
     margin: 0 0.6rem 0 0;
+  }
+  &:hover {
+    background-color: hsl(159, 36%, 21%);
+  }
+  &:active {
+    transform: scale(0.95);
   }
 `;
--- src/components/preview-card/preview.tsx
+++ src/components/preview-card/preview.tsx
@@ -1,11 +1,14 @@
 import MobilePreview from '../../assets/image-product-mobile.jpg';
+import DekstopPreview from '../../assets/image-product-desktop.jpg';
 import Cart from '../../assets/icon-cart.svg';
 import {
   Container,
   Wrapper,
   ImageWrapper,
-  Image,
+  MobileImage,
+  DekstopImage,
   Description,
+  DescWrapper,
   Type,
   Title,
   Info,
@@ -20,23 +23,26 @@ export const Preview = () => {
     <Container>
       <Wrapper>
         <ImageWrapper>
-          <Image src={MobilePreview} />
+          <MobileImage src={MobilePreview} />
+          <DekstopImage src={DekstopPreview} />
         </ImageWrapper>
         <Description>
-          <Type>PERFUME</Type>
-          <Title>Gabrielle Essence Eau De Parfum</Title>
-          <Info>
-            A floral, solar and voluptuous interpretation composed by Olivier
-            Polge, Perfumer-Creator for the House of CHANEL.
-          </Info>
-          <PriceWrapper>
-            <Price>$149.99</Price>
-            <DiscPrice>$169.99</DiscPrice>
-          </PriceWrapper>
-          <Button>
-            <img src={Cart} alt='' />
-            <span> Add to Cart</span>
-          </Button>
+          <DescWrapper>
+            <Type>PERFUME</Type>
+            <Title>Gabrielle Essence Eau De Parfum</Title>
+            <Info>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </Info>
+            <PriceWrapper>
+              <Price>$149.99</Price>
+              <DiscPrice>$169.99</DiscPrice>
+            </PriceWrapper>
+            <Button>
+              <img src={Cart} alt='' />
+              <span> Add to Cart</span>
+            </Button>
+          </DescWrapper>
         </Description>
       </Wrapper>
     </Container>
